aafa1d062166381f9aa452009a0b3dc4f842b00c
AmonDeShir <amonsir29@gmail.com>
2020-08-02 12:05:33 +0200

    init

---
+++ base_block/base_block.gd
@@ -0,0 +1,10 @@
+class_name BaseBlock
+extends Object
+
+
+var durabirity : float
+var mass : float
+var texture : Texture3D
+
+func _init():
+	pass
---
+++ player/player.gd
@@ -0,0 +1,12 @@
+class_name Player
+extends KinematicBody
+
+export var MOVE_SPEED = 10.0
+
+export var slave_position := Vector3()
+export var slave_rotaton := Basis()
+
+func _physics_process(delta):
+	
+	
+
